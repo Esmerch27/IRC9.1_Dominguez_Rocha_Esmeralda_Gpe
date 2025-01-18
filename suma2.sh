451368a56af132bcee7fffb2a3dfb521336a3207
Esmerch27 <edominguez14933@plataforma-utslp.net>
2025-01-18 12:09:22 -0600

    Update suma2.sh

--- suma2.sh
+++ suma2.sh
@@ -9,5 +9,5 @@ read num2
 suma=$((num1 + num2))
 resta=$((num1 - num2))
 
-echo "La suma de $num1 y $num2 es: $resta"
-echo "La resta de $num1 menos $num2 es: $suma"
+echo "La suma de $num1 y $num2 es: $suma"
+echo "La resta de $num1 menos $num2 es: $resta"
