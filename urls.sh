dfeef83f936b598c2ede0e9a01e6acde984812ca
Esmerch27 <edominguez14933@plataforma-utslp.net>
2025-01-30 00:45:10 -0600

    Scrip de urls

---
+++ urls.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+declare -a urls=("http://www.redhat.com/" "https://www.ibm.com/" "https://www.oracle.com")
+
+file=$(mktemp)
+
+printf "$(date)\n" > "$file"
+
+for url in "${urls[@]}"; do
+        status=$(curl -m 10 -s -I $url | head -n 1 | awk '{print $2}')
+        printf "$url,$status\n" >> "$file"
+
+done
+column -s, -t "$file"
+rm -f "$file"
